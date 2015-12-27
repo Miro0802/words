4d37ed3c57418689435a6e14ee2be16b918d69c0
Yu Hsun Lee <andylee0914.tw@gmail.com>
2015-12-27 23:07:18 +0800

    First version of words

---
+++ server.js
@@ -0,0 +1,29 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(8080);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+app.get('/draw1', function (req, res) {
+  res.sendfile(__dirname + '/draw1.html');
+});
+
+app.get('/draw2', function (req, res) {
+  res.sendfile(__dirname + '/draw2.html');
+});
+
+
+io.on('connection', function (socket) {
+    socket.on('draw',function(data){
+        console.log(data);
+        socket.broadcast.emit('draw',data);
+    });
+
+    socket.on('clean',function(data){
+        socket.broadcast.emit('clean',data);
+    });
+});
